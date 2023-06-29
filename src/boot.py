cb4f96077806d1b95814e768709ab5cb4a633d89
Riza Semih Koca <rsemihkoca@outlook.com>
2023-06-29 23:59:49 +0300

    commit

--- src/boot.py
+++ src/boot.py
@@ -4,12 +4,11 @@
 #import webrepl
 #webrepl.start()
 from wifi import WiFiManager
-
+import os
 
 # Wi-Fi credentials
 wifi_ssid = "your_wifi_ssid"
 wifi_password = "your_wifi_password"
-
 
 
 # Instantiate the WiFiManager and MQTTSubscriber classes
@@ -17,3 +16,14 @@ wifi_manager = WiFiManager(wifi_ssid, wifi_password)
 
 # Connect to Wi-Fi, MQTT, run, and disconnect
 wifi_manager.connect()
+
+# Check if the original main.py exists
+if 'main.py.orig' in os.listdir():
+    # Rename main.py.orig back to main.py
+    os.rename('main.py.orig', 'main.py')
+
+# Check if main.py exists
+if 'main.py' in os.listdir():
+    # Run main.py
+    exec(open('main.py').read())
+
